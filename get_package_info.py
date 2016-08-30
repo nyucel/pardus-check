0ca9c4e64a0ea7d39a32fa06d7431538e8ac0a1a
Pain <pain@painkiller-2>
2016-08-30 11:34:50 +0300

    Add Pardus and Debian package lists and diff files. Update get_package_info script

--- get_package_info.py
+++ get_package_info.py
@@ -4,15 +4,11 @@ import re
 def get_package_info(package_info, info):
     return re.search(info+": (.*)\n", package_info).group(1)
 
-def get_package_name(package_info):
-    return get_package_info(package_info, 'Package')
-
-def get_package_version(package_info):
-    return get_package_info(package_info, 'Version')
-
 def get_package_sha1sum(package_info):
     return get_package_info(package_info, 'SHA1')
 
+def get_package_filename(package_info):
+    return './' + '/'.join(get_package_info(package_info, 'Filename').split('/')[2:])
 ###
 
 packages_file_path = sys.argv[1]
@@ -22,8 +18,7 @@ packages_list = packages_file.split('\n\n')
 for package_info in packages_list:
     if len(package_info) < 10:
 	continue
-    package_name = get_package_name(package_info)
-    package_version = get_package_version(package_info)
+    package_filename = get_package_filename(package_info)
     package_sha1sum = get_package_sha1sum(package_info)
-    print package_name, package_sha1sum
+    print package_filename, package_sha1sum
 
